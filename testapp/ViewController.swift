2ecd037fd637c5508385baa70968685b066c16e4
Furkan Yıldız <40992809+Fy1717@users.noreply.github.com>
2023-03-19 18:02:44 +0300

    ui pages done

---
+++ testapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by furkan yıldız on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("furkan")
+        
+        
+        // textLabel.text = "furkan"
+    }
+    
+    
+
+}
+
---
+++ testapp/ViewControllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  testapp
+//
+//  Created by furkan yıldız on 19.03.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
